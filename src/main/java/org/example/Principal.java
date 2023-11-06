14051161be694077c49124e472bd1f0c1425428d
Gubiar <gubiardev@gmail.com>
2023-11-06 15:19:26 -0300

    first commit

---
+++ src/main/java/org/example/Principal.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Principal: envia mensagem para o servidor");
+//String soapEndpointUrl = "http://wscorreio.multilaser.com.br/soap/AtendeCliente.wsdl";
+        String soapEndpointUrl = "https://apps.correios.com.br/SigepMasterJPA/AtendeClienteService/AtendeCliente?wsdl";
+        String soapAction = "http://cliente.bean.master.sigep.bsb.correios.com.br";
+        SoapClient sc = new SoapClient();
+//Requisicao1
+        System.out.println("\n---Requisicao1---");
+        sc.setCEP("86812460");
+        sc.callSoapWebService(soapEndpointUrl, soapAction);
+
+        System.out.println("Fim!");
+    }
+}
